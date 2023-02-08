81639de57d46acf8e56d99bf42bd83888dfb98d4
Alex Trbovic <alextrbovic@outlook.com>
2023-02-07 20:56:03 -0500

    Minor change

--- main.js
+++
@@ -1,6 +0,0 @@
-const projectsButton = document.querySelector("#projects-button");
-const projectsPage = document.querySelector("#projects-page");
-
-projectsButton.addEventListener("click", function() {
-  projectsPage.style.display = "block";
-});
