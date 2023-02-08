047d82f5614df58c5359765a90453f8c1c209733
Alex Trbovic <alextrbovic@outlook.com>
2023-02-07 20:50:40 -0500

    Linking the pages together

---
+++ main.js
@@ -0,0 +1,6 @@
+const projectsButton = document.querySelector("#projects-button");
+const projectsPage = document.querySelector("#projects-page");
+
+projectsButton.addEventListener("click", function() {
+  projectsPage.style.display = "block";
+});
